edec8e4eebfbf5de0f5d127cb394a80f9ef48dab
smhmd <contact.simohamed@gmail.com>
2026-09-14 14:10:04 +0100

    chore: remove typing-test

--- src/apps/typing-test/components/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './Result'
-export * from './StickyNote'
-export * from './Timer'
-export * from './TypingArea'
-export * from './WavyText'
--- src/apps/typing-test/lib/common.ts
+++
@@ -1,3 +0,0 @@
-export const sentences = `The quick brown fox jumps over the lazy dog. Every morning, she walks to the market to buy fresh vegetables and fruits. Although the weather was cold, he decided to take the long route, passing by the river where children were playing. Don’t forget to check the mail, or important letters may be missed.`
-
-export const FONT = ''
